961e938334528644834ae0ff8ab2abb53ba957ab
Pongstr <twiz.tickler@gmail.com>
2022-10-06 17:57:16 +0300

    setup the models we're going to work with

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,25 @@
 import React from 'react'
+import * as collection from './data/users.json'
+import './App.css'
+
+type SelectedUserModelType = UserModelType & { index: number }
+type SelectedUsersActionType = {
+  type: 'add' | 'remove' | 'add-all' | 'remove-all'
+  payload: SelectedUserModelType | SelectedUserModelType[]
+}
+
+type UserModelType = {
+  uid: string
+  name: string
+  selected: boolean
+  description: string
+  link?: string
+}
+
+type CollectionListPropType = {
+  collection: UserModelType[]
+  render: (model: UserModelType, index: number) => React.ReactElement
+}
 
 /**
  * Root App
